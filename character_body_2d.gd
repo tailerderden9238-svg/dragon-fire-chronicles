f76d742b8a4cb1b705bde586efe3e50c5169b090
tailerderden <tailerderden9238@gmail.com>
2026-01-08 17:07:46 +0300

    иницмлмзация проекта

---
+++ character_body_2d.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+@onready var anim = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		anim.play("Jump")
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+		if velocity.y == 0:
+			anim.play("Run")
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if velocity.y == 0:
+			anim.play("Idle")
+	
+	if direction == -1:
+		$AnimatedSprite2D.flip_h = true	 
+	elif direction == 1:
+		$AnimatedSprite2D.flip_h = false
+		
+	if velocity.y > 0:
+		anim.play("Fall")
+	
+	move_and_slide()
